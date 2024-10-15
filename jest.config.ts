80dc54e75cc5cd1932c9ef3a7dc01e06c46aa1c0
Elvis Le <elvishaile120@gmail.com>
2024-10-16 03:24:23 +1100

    added unit testing with jest

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {Config} from 'jest'
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+const config: Config = {
+
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+
+};
+
+export default createJestConfig(config);
---
+++ src/utils/add.test.js
@@ -0,0 +1,5 @@
+import add from './add';
+
+test('adds 20 + 30 to equal 50', () => {
+    expect(add(20, 30)).toBe(50);
+  });
